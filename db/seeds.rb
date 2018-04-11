d8d28661e850a62bf9d989c13d7784bfe6c115e4
deepgpatel <deepgpatel93@gmail.com>
2018-04-11 09:09:46 -0400

    Done.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+school_class = SchoolClass.create([{title:'idk' , room_number:5}])
+school_class
